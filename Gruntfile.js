03e5bc137b33e3239cadcac779a7aa7332dd5313
Eli Mattson <elimattson13@gmail.com>
2015-05-26 20:39:10 -0700

    add grunt and main.js

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-browserify');
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        express: {
+            options: {
+                // Override defaults here
+            },
+            dev: {
+                options: {
+                    script: 'bin/www',
+                    port:3000
+                }
+            }
+        },
+        browserify: {
+            main: {
+                browserifyOptions: {
+                    debug: true
+                },
+                src: 'js/main.js',
+                dest: 'public/javascripts/main.js'
+            }
+        },
+        watch: {
+            js: {
+                files: 'js/*.js',
+                tasks: ['default']
+            },
+            jade: {
+                files: '**/*.jade',
+                tasks: ['reload']
+            }
+        }
+    });
+    grunt.registerTask('reload', ['express:dev', 'watch']);
+    grunt.registerTask('default', ['browserify', 'reload']);
+
+};
